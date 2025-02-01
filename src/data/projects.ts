cbf4cc895036c1bfd8f50e04b7425c306f827a6c
Sebastian Velasquez <anse23@hotmail.com>
2025-02-01 16:26:57 -0500

    data: Completes projects content

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,98 +1,180 @@
 export default [
   {
-    name: 'WGSN',
+    name: 'WGSN - CMS',
     type: 'Web',
-    imgUrl: '/images/projects/wgsn.jpg',
-    technologies: ['React', 'UX'],
+    imgUrl: '/images/projects/stream-cms.jpg',
+    technologies: ['React', 'Typescript'],
     links: {
-      github: 'https://github.com',
-      web: 'https://google.com',
+      web: 'https://www.wgsn.com/stream-cms',
     },
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.',
+      'Developed a custom CMS using React-Admin, designed for seamless content creation and management. The platform enables authors to efficiently produce visually rich articles combining images and text, ensuring fast and engaging content consumption. Optimized for intuitive use, the system streamlines editorial workflows, making high-quality content easily accessible to readers.',
   },
   {
     name: 'Apollo Podcast',
     type: 'Mobile',
-    imgUrl: '/images/projects/wgsn.jpg', // Update this
-    technologies: ['React', 'UX'],
+    imgUrl: '/images/projects/apollo.png',
+    technologies: ['React Native'],
     links: {
-      github: 'https://github.com',
-      web: 'https://google.com',
+      web: 'https://apollopods.com/',
     },
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.',
+      'Contributed to the development of a mobile app for fictional podcasts, providing an immersive audio storytelling experience. Built with React Native, the app offers a seamless interface for discovering, streaming, and managing fictional audio series. Focused on usability and engagement, the platform enhances storytelling with dynamic UI elements and smooth navigation for an enjoyable listening experience.',
   },
   {
     name: 'VPS-MSRS',
     type: 'Mobile',
-    imgUrl: '/images/projects/wgsn.jpg', // Update this
-    technologies: ['React', 'UX'],
-    links: {
-      github: 'https://github.com',
-      web: 'https://google.com',
-    },
+    imgUrl: '/images/projects/msrs.png',
+    technologies: ['React Native', 'UX'],
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.',
+      'Designed and developed a mobile application from scratch for a multi-source reckoning system using React Native. The app integrates data from various sources to provide accurate positioning and tracking. Focused on performance and usability, it delivers real-time insights with a seamless user experience, making navigation and location-based decision-making more efficient and reliable.',
   },
   {
     name: 'Vision',
     type: 'Web',
-    imgUrl: '/images/projects/wgsn.jpg', // Update this
-    technologies: ['React', 'UX'],
+    imgUrl: '/images/projects/landing3d.jpg',
+    technologies: ['Reactjs', 'Threejs'],
     links: {
-      github: 'https://github.com',
-      web: 'https://google.com',
+      web: 'https://sosegon.github.io/landing3d/',
     },
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.',
+      'Designed and developed a visually captivating landing page with interactive 3D elements using React.js and Three.js. Built from scratch, the project leveraged advanced web technologies to create an immersive and dynamic user experience, seamlessly blending modern design with real-time 3D interactivity to enhance user engagement.',
   },
   {
     name: 'Mango grading',
     type: 'Computer Vision',
-    imgUrl: '/images/projects/wgsn.jpg', // Update this
-    technologies: ['React', 'UX'],
+    imgUrl: '/images/projects/mango.jpg',
+    technologies: ['Python', 'OpenCV'],
     links: {
-      github: 'https://github.com',
-      web: 'https://google.com',
+      github: 'https://github.com/sosegon/mango-grading',
+      web: 'https://ieeexplore.ieee.org/abstract/document/6968366',
     },
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.',
+      'Developed an image analysis system to assess the health, maturity, and quality of mangoes. The project focused on processing and evaluating color properties across different color spaces, enabling accurate classification and quality control. By leveraging advanced image processing techniques, the solution provided valuable insights for agricultural and commercial applications.',
   },
   {
-    name: 'Mango grading',
-    type: 'Computer Vision',
-    imgUrl: '/images/projects/wgsn.jpg', // Update this
-    technologies: ['React', 'UX'],
+    name: 'Farmpolitik',
+    type: 'Web',
+    imgUrl: '/images/projects/farmpolitik.jpg',
+    technologies: ['Reactjs', 'Threejs', 'Firebase'],
     links: {
-      github: 'https://github.com',
-      web: 'https://google.com',
+      github: 'https://github.com/sosegon/farmpolitik',
+      web: 'https://farmpolitik.com',
     },
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.',
+      'Designed and developed a 3D board game for two players, combining strategic gameplay with real-time interaction. Built from scratch using React.js, Three.js, and Firebase, the project featured an immersive 3D environment and an integrated chatroom, enhancing player engagement. The implementation focused on seamless real-time updates and smooth user experience, making for an interactive and dynamic multiplayer game.',
   },
   {
-    name: 'Mango grading',
-    type: 'Computer Vision',
-    imgUrl: '/images/projects/wgsn.jpg', // Update this
-    technologies: ['React', 'UX'],
+    name: 'AnalyzeCeph',
+    type: 'Web',
+    imgUrl: '/images/projects/analyzeceph.jpg',
+    technologies: ['Reactjs'],
     links: {
-      github: 'https://github.com',
-      web: 'https://google.com',
+      web: 'https://www.analyzeceph.com/',
     },
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.',
+      'Developed a web application for cephalometric analysis using React.js, enhancing functionality and usability. Improved an existing codebase by implementing new features and creating comprehensive documentation from scratch. The project focused on increasing accuracy, maintainability, and user experience, streamlining cephalometric assessments for professionals in the field.',
   },
   {
-    name: 'Mango grading',
-    type: 'Computer Vision',
-    imgUrl: '/images/projects/wgsn.jpg', // Update this
-    technologies: ['React', 'UX'],
+    name: 'Numbers',
+    type: 'Web',
+    imgUrl: '/images/projects/numbers.png',
+    technologies: ['Reactjs'],
     links: {
-      github: 'https://github.com',
-      web: 'https://google.com',
+      github: 'https://github.com/sosegon/numbers',
+      web: 'https://sosegon.github.io/numbers-demo/',
     },
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.',
+      'Designed and developed a casual single-player game featuring AI-driven opponents, built from the ground up using React.js and Redux. This personal project focused on delivering a smooth and engaging gameplay experience while incorporating structured documentation to ensure maintainability and future enhancements. The game balances simplicity and strategy, offering an accessible yet compelling challenge for players.',
+  },
+  {
+    name: 'Plates counting',
+    type: 'Computer Vision',
+    imgUrl: '/images/projects/plates.jpg', // Update this
+    technologies: ['Python', 'OpenCV'],
+    links: {
+      github: 'https://github.com/sosegon/plates-counting',
+    },
+    description:
+      'Developed a computer vision-based software solution to analyze video clips of a manufacturing process, implementing algorithms for object tracking, intensity analysis, and thresholding. Built with Python and OpenCV, the system ensures precise and efficient video processing, enabling better monitoring and optimization of production workflows.',
+  },
+  {
+    name: 'AnkiGame',
+    type: 'Mobile',
+    imgUrl: '/images/projects/anki.jpg', // Update this
+    technologies: ['Android', 'Reactjs', 'Firebase'],
+    links: {
+      github: 'https://github.com/sosegon/AnkiGame',
+    },
+    description:
+      'Developed a gamified version of the educational tool AnkiDroid, involving the evaluation of the existing user interface, design of a gamification strategy, and implementation of the solution. Utilizing Firebase for storage and React.js for frontend development, the project incorporated a casual game as an additional motivational element to enhance user engagement.',
+  },
+  {
+    name: 'Skin filter',
+    type: 'Web',
+    imgUrl: '/images/projects/skin-filter.jpg', // Update this
+    technologies: ['Chrome', 'Javascript'],
+    links: {
+      github: 'https://github.com/sosegon/OS-Image-Filter',
+      web: 'https://chrome.google.com/webstore/detail/skin-filter/digkbjcglmbbipijiogdeegbfbepgdon/related?hl=en-GB',
+    },
+    description:
+      'Developed a Chrome extension aimed at detecting and filtering human skin in website images to protect vulnerable audiences from explicit content. This project involved adapting and extending an existing extension by implementing computer vision algorithms and probabilistic analysis in the source code. The objective of the tool is to enhance online safety by intelligently concealing potentially inappropriate content.',
+  },
+  {
+    name: 'Remove BG',
+    type: 'Web',
+    imgUrl: '/images/projects/remove-bg.jpg', // Update this
+    technologies: ['Chrome', 'Javascript'],
+    links: {
+      github:
+        'https://github.com/sosegon/udacity-projects/tree/master/machine-learning-remove-background',
+    },
+    description:
+      'Developed a desktop application utilizing Python and OpenCV to remove backgrounds from brick imagery, employing machine learning algorithms to analyze image content and extract essential information. This solution significantly enhanced workflow efficiency in texture creation processes.',
+  },
+  {
+    name: 'SelfCad',
+    type: 'Web',
+    imgUrl: '/images/projects/selfcad.jpg', // Update this
+    technologies: ['GWT', 'Angular'],
+    links: {
+      web: 'http://www.selfcad.com/beta-20/app/index.html',
+    },
+    description:
+      'Designed and developed a web application using GWT and Angular for creating 3D-printable objects, accommodating both beginners and experienced users. Implemented over 20 advanced algorithms to enhance functionality and precision. Additionally, led a team of six, managing development processes and ensuring seamless collaboration to deliver a powerful and user-friendly design tool.',
+  },
+  {
+    name: 'Repetitive textures',
+    type: 'Image',
+    imgUrl: '/images/projects/textures.jpg', // Update this
+    technologies: ['Photoshop'],
+    links: {
+      web: 'https://3dwarehouse.sketchup.com/collection.html?id=eba75af9-5475-4a04-927f-9b075d835ad8',
+    },
+    description:
+      'Crated a collection of 155 seamless textures for 3D content by meticulously processing 360 raw images of brick patterns. This involved cleaning, editing, and optimizing images to ensure high-quality, tileable results. Additionally, designed 22 unique layouts to enhance visual diversity, offering a versatile and realistic texture library for various applications.',
+  },
+  {
+    name: '3D Campus',
+    type: 'Web',
+    imgUrl: '/images/projects/campus.jpg', // Update this
+    technologies: ['Sketchup', 'Javascript'],
+    links: {
+      web: 'http://www2.ucsg.edu.ec/campus_virtual/index_campus.php',
+    },
+    description:
+      "Developed a web application offering a virtual tour of one of Guayaquil's primary universities in Ecuador, with the goal of providing an intuitive platform for visualizing the university's assets. Tasks involved creating 3D models for 22 university buildings, compiling the 3D environment, and deploying the application for web access, ensuring seamless navigation and exploration.",
+  },
+  {
+    name: 'Net Tracking',
+    type: 'Web',
+    imgUrl: '/images/projects/net.jpg', // Update this
+    technologies: ['ASP.NET'],
+    links: {
+      web: 'http://goo.gl/uJbIWn',
+    },
+    description:
+      'Developed a robust web application tasked with collecting and analyzing data from devices within local networks. The application processes and presents this information in a user-friendly interface, empowering administrators to make informed decisions based on the insights provided.',
   },
 ];
